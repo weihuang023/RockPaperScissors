360dace1989efbf095aa2c9d5b931f61387d0c98
weihuang023 <weih81888@gmail.com>
2016-02-17 07:45:09 -0500

    add rock paper scissor in the activity main

--- app/src/main/java/com/example/huang/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/huang/rockpaperscissors/MainActivity.java
@@ -9,10 +9,12 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     ImageButton imageButton;
     ImageView imageView;
-
+    ImageView imageView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,17 +23,45 @@ public class MainActivity extends AppCompatActivity {
         aboutButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,About.class);
+                Intent intent = new Intent(MainActivity.this, About.class);
                 startActivity(intent);
             }
         });
 
-        imageView = (ImageView) findViewById(R.id.imageView2);
+        final int[] photos = {R.drawable.paper,R.drawable.rock,R.drawable.scissor};
+        Random ran = new Random();
+        final int i = ran.nextInt(photos.length);
+        imageView1 = (ImageView) findViewById(R.id.computer_imageview);
+        imageButton = (ImageButton) findViewById(R.id.start_imagebutton);
+        imageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView1.setImageResource(photos[i]);
+            }
+        });
+
+        imageView = (ImageView) findViewById(R.id.human_imageview);
         imageButton = (ImageButton)findViewById(R.id.rock_imagebutton);
         imageButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 imageView.setImageResource(R.drawable.rock);
+            }
+        });
+
+        imageButton = (ImageButton)findViewById(R.id.paper_imagebutton);
+        imageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.drawable.paper);
+            }
+        });
+
+        imageButton = (ImageButton)findViewById(R.id.scissor_imagebutton);
+        imageButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setImageResource(R.drawable.scissor);
             }
         });
     }
